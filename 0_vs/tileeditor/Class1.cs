af71cc50d54e4385fbed69861eec45a41e09a53a
Vincent Mahnke <vincent@mahn.ke>
2019-01-07 02:08:24 +0100

    Initial commit

---
+++ 0_vs/tileeditor/Class1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace tileeditor
+{
+    enum TileType : byte
+    {
+        Goal = (byte)'G',
+        Wall = (byte)'*',
+        Empty = (byte)'0',
+        Start = (byte)'S',
+        Fruit = (byte)'P',
+        Unknown = (byte)'F'
+    }
+
+    struct Tile
+    {
+        private TileType type;
+        private string index;
+
+        public static Tile Load(BinaryReader reader)
+        {
+            Tile tile = new Tile();
+            tile.type = (TileType)reader.ReadByte();
+            tile.index = System.Text.Encoding.Default.GetString(reader.ReadBytes(3));
+            return tile;
+        }
+
+        public char Visualize()
+        {
+            return type == TileType.Empty ? ' ' : (char)type;
+        }
+    }
+
+    struct MapData
+    {
+        const int ROWS_VISIBLE = 15;
+        const int ROWS_TOTAL = 20;
+        const int COLUMNS_VISIBLE = 18;
+        const int COLUMNS_TOTAL = 24;
+        #region Header
+        private byte[] headerUnknown;
+        private byte[] background;
+        private byte[] headerUnknown2;
+        #endregion
+
+        #region Payload
+        private Tile[,] visibleRows;
+        private Tile[,] invisibleRows;
+        #endregion
+
+        public static MapData Load(string fileName)
+        {
+            MapData mapData = new MapData();
+            using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            {
+                mapData.headerUnknown = reader.ReadBytes(16);
+                mapData.background = reader.ReadBytes(4);
+                mapData.headerUnknown2 = reader.ReadBytes(114);
+                mapData.visibleRows = new Tile[ROWS_VISIBLE, COLUMNS_TOTAL];
+                for (int row = 0; row < ROWS_VISIBLE; row++)
+                {
+                    for (int column = 0; column < COLUMNS_TOTAL; column++)
+                    {
+                        mapData.visibleRows[row,column] = Tile.Load(reader);
+                    }
+                }
+                mapData.invisibleRows = new Tile[ROWS_TOTAL-ROWS_VISIBLE, COLUMNS_TOTAL];
+                for (int row = 0; row < ROWS_TOTAL - ROWS_VISIBLE; row++)
+                {
+                    for (int column = 0; column < COLUMNS_TOTAL; column++)
+                    {
+                        mapData.invisibleRows[row,column] = Tile.Load(reader);
+                    }
+                }
+            }
+            return mapData;
+        }
+
+        public string Visualize()
+        {
+            string result = "";
+            result += ' ';
+            result += ' ';
+            result += ' ';
+
+            for (int column = 0; column < COLUMNS_TOTAL; column++)
+            {
+                result += (column < COLUMNS_VISIBLE) ? 'V' : 'I';
+            }
+            result += " \r\n";
+            result += " \r\n";
+            result += " \r\n";
+
+            for (int row = 0; row < ROWS_VISIBLE; row++)
+            {
+                result += 'V';
+                result += ' ';
+                result += ' ';
+                for (int column = 0; column < COLUMNS_TOTAL; column++)
+                {
+                    result += this.visibleRows[row, column].Visualize();
+                }
+                result += " \r\n";
+            }
+            for (int row = 0; row < ROWS_TOTAL - ROWS_VISIBLE; row++)
+            {
+                result += ('I');
+                result += ' ';
+                result += ' ';
+                for (int column = 0; column < COLUMNS_TOTAL; column++)
+                {
+                    result += this.invisibleRows[row, column].Visualize();
+                }
+                result += " \r\n";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ 0_vs/tileeditor/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace tileeditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            // detect remaining temp-folder
+            InitializeComponent();
+        }
+
+        ~MainWindow()
+        {
+            // detect remaiming temp-folder and delete it
+        }
+
+        bool MoveAndExtract(string filePath)
+        {
+            string newTargetFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/tmp/sourceFiles/";
+            Directory.CreateDirectory(newTargetFolder);
+            File.Copy(filePath, newTargetFolder + Path.GetFileName(filePath), true);
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "py",
+                    Arguments = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/Scripts/SARC/SARCExtract.py " + newTargetFolder + Path.GetFileName(filePath),
+                    CreateNoWindow = true
+                }
+            };
+            process.Start();
+            process.WaitForExit();
+
+            string extractedFolderName = newTargetFolder + Path.GetFileNameWithoutExtension(filePath) + "/";
+            DirectoryInfo dirInfo = new DirectoryInfo(extractedFolderName);
+            return dirInfo.Exists;
+        }
+
+        private void LoadFile_Click(object sender, RoutedEventArgs e)
+        {
+            // open and extract SZS
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Yoshi's package file (Ysi_Cmn.pack)|Ysi_Cmn.pack";
+            if (openFileDialog.ShowDialog() != true)
+            {
+                // no file selected
+                return;
+            }
+
+            if (!MoveAndExtract(openFileDialog.FileName))
+            {
+                throw new System.Exception();
+            }
+
+            if (!MoveAndExtract(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/tmp/sourceFiles/Ysi_Cmn/Common/Scene/Ysi.szs"))
+            {
+                throw new System.Exception();
+            }
+
+            PopulateList();
+        }
+
+        void PopulateList()
+        {
+            IEnumerable<string> maps = Directory.EnumerateFiles(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "tmp", "sourceFiles", "Ysi"), "MapData*.exbin");
+            LevelSelector.Items.Clear();
+            foreach (string map in maps)
+            {
+                LevelSelector.Items.Add(Path.GetFileNameWithoutExtension(map));
+            }
+        }
+
+        private void ParseFile_Click(object sender, RoutedEventArgs e)
+        {
+            // open and parse .exbin
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "tmp", "sourceFiles", "Ysi");
+            openFileDialog.Filter = "Extracted binary-files (*.exbin)|*.exbin";
+            if (openFileDialog.ShowDialog() != true)
+            {
+                // no file selected
+                return;
+            }
+
+        }
+
+        private void LevelSelector_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            if (e.AddedItems.Count <= 0)
+            {
+                return;
+            }
+
+            using (var memStream = new MemoryStream())
+            {
+                MapData mapData = MapData.Load(Path.Combine(
+                                      Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location),
+                                      "tmp",
+                                      "sourceFiles",
+                                      "Ysi",
+                                      (e.AddedItems[0] as string) + ".exbin"
+                                  ));
+                LevelData.Text = mapData.Visualize();
+            }
+        }
+    }
+}
